3fe713076520541eeec61a736daec2c91824fc69
Gaston <armijogaston@gmail.com>
2025-11-26 11:28:58 -0300

    se corigio error de product tesoro

--- js/carrito.js
+++ js/carrito.js
@@ -26,7 +26,7 @@ const renderizarCarrito = () => {
         tarjeta.classList.add("tarjeta-producto");
         
         const img = document.createElement("img");
-        img.src = `../${producto.img}`;
+        img.src = `./${producto.img}`;
         img.alt = producto.nombre;
 
         const titulo = document.createElement("h3");
